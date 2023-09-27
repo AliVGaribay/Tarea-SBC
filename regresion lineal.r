9296218b168a865a0e2188aa2acbcdd78638d169
AliVGaribay <144314010+AliVGaribay@users.noreply.github.com>
2023-09-26 21:49:13 -0600

    Add files via upload

--- regresion lineal.r
+++ regresion lineal.r
@@ -7,23 +7,31 @@ regresionLineal <- function(x,y){
 	dividendo <- 0
 	divisor <- 0
 
+	#se obtiene el resultado de las sumatorias
 	for(c in 1:length(x)){
 		dividendo <- dividendo + (x[c] - xMedia) * (y[c] - yMedia)
 		divisor <- divisor + (x[c] - xMedia) * (x[c] - xMedia)
 	}
 	
+	#Se calcula m (pendiente)
 	m <- dividendo / divisor
+	#se calcula b (origen)
 	b <- yMedia - (m * xMedia)
 	
+	#Se prepara la salida del retorno
 	theta <- c(b,m)
 	
 	return(theta)
 }
 
+#Se lee el dataset
 datos <- read.csv("Salary_dataset.csv")
 
+#Se aplica la funcion sobre el dataset
 theta <- regresionLineal(datos$YearsExperience, datos$Salary)
 
+#Se grafican los puntos
 plot(datos$YearsExperience, datos$Salary, col = "blue", pch = 19)
 
+#Se grafica la funcion obtenida de la regresion lineal
 abline(a = theta[1], b = theta[2], col = "red", lwd = 3)
